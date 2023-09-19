716473c8ac28eeb00953201a3d67d39a3f8acc05
Oyiti <emmanueloyiti2@gmail.com>
2023-09-19 16:09:37 +0000

    ReCommit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contactForm');
+
+    contactForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        alert(`Email: ${email}\nSubject: ${subject}\nMessage: ${message}`);
+		alert('Thank you for your submission!');
+		contactForm.reset();
+    });
+});
